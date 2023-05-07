7434500099dceeeebf0c9b1b7c4dc7bcc18c0d72
David Charles <charled1@gmatc.matc.edu>
2023-05-07 16:54:19 -0500

    Final P6 complete

--- blog/src/components/header/Header.js
+++ blog/src/components/header/Header.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import styled from "styled-components"
 import { H1 } from '../Heading'
+import { Section } from '../Section'
 
 const Outer = styled.header`
   background: ${props => props.theme.header.backgroundColor};
@@ -24,11 +25,16 @@ const StyledLink = styled(Link)`
 
 const Header = ({ siteTitle }) => (
   <Outer>
+  <Section width = {11/12}>
     <H1>
       <StyledLink to="/">
         {siteTitle}
       </StyledLink>
     </H1>
+    </Section>
+    <Section>
+      Search
+    </Section>
   </Outer>
 )
 
