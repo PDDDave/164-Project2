b86106cb736d3e6d4e9791b864dc1529ddf50ae4
David Charles <charled1@gmatc.matc.edu>
2023-05-07 13:47:34 -0500

    index updated

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -13,6 +14,14 @@ const IndexPage = ({ data }) => (
         data.allContentfulDexEntry.edges.map(edge => (
           <li key={edge.node.id}>
             <Link to={edge.node.slug}>#{edge.node.monsterNumber} - {edge.node.pokemonName}</Link>
+            <div>
+              <GatsbyImage
+                image={edge.node.heroImage.gatsbyImageData}
+              />
+            </div>
+            <div>
+              {edge.node.description.childMarkdownRemark.excerpt}
+            </div>
           </li>
         ))
 
@@ -26,7 +35,7 @@ export const Head = () => <Seo title="Home" />
 export default IndexPage
 
 export const query = graphql `
-    {
+{
       allContentfulDexEntry {
         edges{
           node{
@@ -34,6 +43,18 @@ export const query = graphql `
             monsterNumber
             pokemonName
             slug
+            description{
+              childMarkdownRemark{
+                excerpt
+              }
+            }
+            heroImage{
+              gatsbyImageData(
+                layout: CONSTRAINED
+                placeholder: BLURRED
+                width:300
+              )
+            }
           }
         }
       }
